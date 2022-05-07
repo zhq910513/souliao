d36cfa97e913cce0fb7d6310514ed19878db2b4e
zhang513 <zhq00000@gmail.com>
2022-05-07 17:28:01 +0800

    no message

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ print(text_unquote(test))
 
 
 'frida -U -f io.dcloud.W2Awww.soliao.com -l D:/Projects/souliao/cipher.js --no-pause'
-'adb logcat | io.dcloud.W2Awww.soliao.com >D:\log.txt'
+'adb logcat | findstr io.dcloud.W2Awww.soliao.com >D:\log.txt'
 
 
 """
