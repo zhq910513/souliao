826c9352f8f9d468a349269093f45f3a685106d7
zhang513 <657697084@qq.com>
2022-05-07 15:21:55 +0800

    no message

--- cipher.js
+++ cipher.js
@@ -1,49 +1,175 @@
-function main() {
+Java.perform(function() {
+    //Base64
+    var base64=Java.use('android.util.Base64');
+    var string=Java.use('java.lang.String');
+    /*base64.encode.overload('[B', 'int', 'int', 'int').implementation = function(){
+        send("=================base64 encode====================");
+        send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+        send(arguments[0]);
+        send(arguments[1]);
+        send(arguments[2]);
+        send(arguments[3]);
+        var data=this.encode(arguments[0],arguments[1],arguments[2],arguments[3])
+        send("base64:"+string.$new(data));
+        return data;
+    }*/
 
-if (Java.available) {
+    /*base64.decode.overload('[B', 'int', 'int', 'int').implementation = function(){
+        send("=================base64 decode====================");
+        send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+        send(arguments[0]);
+        send(arguments[1]);
+        send(arguments[2]);
+        send(arguments[3]);
+        var data=this.decode(arguments[0],arguments[1],arguments[2],arguments[3])
+        send("base64:"+string.$new(data));
+        return data;
+    }*/
 
-Java.perform(function(){
 
-const Cipher = Java.use('javax.crypto.Cipher');
+    // MD SHA
+    var messageDigest=Java.use('java.security.MessageDigest');
+    // update
+    for(var i = 0; i < messageDigest.update.overloads.length; i++){
+        messageDigest.update.overloads[i].implementation = function(){
+            var name=this.getAlgorithm()
+            send("================="+name+"====================");
+            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+            if(arguments.length == 1){
+                send(arguments[0]);
+                this.update(arguments[0]);
+            }else if(arguments.length == 3){
+                send(arguments[0]);
+                send(arguments[1]);
+                send(arguments[2]);
+                this.update(arguments[0],arguments[1],arguments[2]);
+            }
+        }
+    }
+    // digest
+    for(var i = 0; i < messageDigest.digest.overloads.length; i++){
+        messageDigest.digest.overloads[i].implementation = function(){
+            var name=this.getAlgorithm()
+            send("================="+name+"====================");
+            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+            if(arguments.length == 0){
+                var data=this.digest();
+                send(data);
+                return data;
+            }else if(arguments.length == 1){
+                send(arguments[0]);
+                var data=this.digest(arguments[0]);
+                send(data);
+                return data;
+            }else if(arguments.length == 3){
+                send(arguments[0]);
+                send(arguments[1]);
+                send(arguments[2]);
+                var data=this.digest(arguments[0],arguments[1],arguments[2]);
+                send(data);
+                return data;
+            }
+        }
+    }
 
-Cipher.init.overload('int', 'java.security.Key', 'java.security.spec.AlgorithmParameterSpec').implementation = function (opmode, key, iv){
+    //MAC
+    var mac=Java.use('javax.crypto.Mac');
+    for(var i = 0; i < mac.doFinal.overloads.length; i++){
+        mac.doFinal.overloads[i].implementation = function(){
+            var name=this.getAlgorithm()
+            send("================="+name+"====================");
+            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+            if(arguments.length == 0){
+                var data=this.doFinal();
+                send(data);
+                return data;
+            }else if(arguments.length == 1){
+                send(arguments[0]);
+                var data=this.doFinal(arguments[0]);
+                send(data);
+                return data;
+            }else if(arguments.length == 2){
+                send(arguments[0]);
+                send(arguments[1]);
+                var data=this.doFinal(arguments[0],arguments[1]);
+                send(data);
+                return data;
+            }
+        }
+    }
 
-if (iv.$className == 'javax.crypto.spec.IvParameterSpec'){
+    // DES DESede AES PBE RSA
+    var cipher=Java.use('javax.crypto.Cipher');
+     for(var i = 0; i < cipher.doFinal.overloads.length; i++){
+        cipher.doFinal.overloads[i].implementation = function(){
+            var name=this.getAlgorithm()
+            send("================="+name+"====================");
+            send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+            if(arguments.length == 0){
+                var data=this.doFinal();
+                send(data);
+                return data;
+            }else if(arguments.length == 1){
+                send(arguments[0]);
+                var data=this.doFinal(arguments[0]);
+                send(data);
+                return data;
+            }else if(arguments.length == 2){
+                send(arguments[0]);
+                send(arguments[1]);
+                var data=this.doFinal(arguments[0],arguments[1]);
+                send(data);
+                return data;
+            }else if(arguments.length == 3){
+                send(arguments[0]);
+                send(arguments[1]);
+                send(arguments[2]);
+                var data=this.doFinal(arguments[0],arguments[1],arguments[2]);
+                send(data);
+                return data;
+            }else if(arguments.length == 5){
+                send(arguments[0]);
+                send(arguments[1]);
+                send(arguments[2]);
+                send(arguments[3]);
+                send(arguments[4]);
+                var data=this.doFinal(arguments[0],arguments[1],arguments[2],arguments[3],arguments[4]);
+                send(data);
+                return data;
+            }else{
+                send(arguments[0]);
+                send(arguments[1]);
+                send(arguments[2]);
+                send(arguments[3]);
+                var data=this.doFinal(arguments[0],arguments[1],arguments[2],arguments[3]);
+                send(data);
+                return data;
+            }
+        }
+    }
 
-var opmodestring = this.getOpmodeString(opmode);
-
-var algo = this.getAlgorithm();
-
-const keyclass = Java.use('javax.crypto.spec.SecretKeySpec');
-
-const ivclass = Java.use('javax.crypto.spec.IvParameterSpec');
-
-var keystoreKey = Java.cast(key, keyclass);
-
-var ivInstance = Java.cast(iv,ivclass);
-
-var result=this.init(opmode,key,iv);
-
-console.log('[] opmode:'+opmodestring);
-
-console.log('[] algorithm:'+algo);
-
-console.log('[] key className:'+key.$className);
-
-console.log('[] key value:'+ JSON.stringify(keystoreKey.getEncoded());
-
-console.log('[] iv value:'+JSON.stringify(ivInstance.getIV());
-
-}
-
-return result;
-
-}
-
-})
-
-}
-
-}
-
-setImmediate(main)
+    //KEY
+    var secretKey=Java.use('javax.crypto.spec.SecretKeySpec');
+    for(var i = 0; i < secretKey.$init.overloads.length; i++){
+        secretKey.$init.overloads[i].implementation = function(){
+            var name=this.getAlgorithm()
+            send("=================KEY====================");
+            //send(Java.use("android.util.Log").getStackTraceString(Java.use("java.lang.Throwable").$new()));
+            if(arguments.length == 2){
+                send(arguments[0]);
+                send(arguments[1]);
+                this.$init(arguments[0],arguments[1]);
+            }else if(arguments.length == 4){
+                send(arguments[0]);
+                send(arguments[1]);
+                send(arguments[2]);
+                send(arguments[3]);
+                this.$init(arguments[0],arguments[1],arguments[2],arguments[3]);
+            }
+        }
+    }
+    //IV
+    //DES KEY
+    //DESede KEY
+    //PBE KEY salt
+});
