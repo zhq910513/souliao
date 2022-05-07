2cbb6951e7e22ff4a25a6f459cfcece96873236e
zhang513 <zhq00000@gmail.com>
2022-05-07 18:28:18 +0800

    no message

--- TechnologyCenterDetailPresenter.js
+++ TechnologyCenterDetailPresenter.js
@@ -2,7 +2,7 @@ Java.perform(function(){
     console.warn("*** Start Hook ***")
 
     var hookSig = Java.use('f.a.a.a.a.l.a.Fd');
-    hookSig.a.implementation = function(arr){
+    hookSig.a.overload('java.util.HashMap').implementation = function(arr){
         var Map = Java.use('java.util.HashMap');
         console.error("************* args_map 传入值 *************")
         var args_map = Java.cast(arr, Map);
